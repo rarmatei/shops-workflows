150d75d0f458bfd5adc8aa7a835ba42ff18608c8
Rares Matei <matei.rar@gmail.com>
2023-11-15 17:56:15 +0000

    add script

---
+++ create-many.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Start the loop from 2 to 31 (since you want to execute it 30 times starting from 2)
+for i in {2..31}
+do
+  # Execute the command with the current value of i
+  nx g @nx/angular:application --name shop-$i --no-routing --bundler webpack --no-ssr --projectNameAndRootFormat=derived
+done
